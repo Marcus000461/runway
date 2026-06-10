ea7eff576ab6d60feea0106f499aa1370f0fb4c8
Caio <antunes.caiogabriel@gmail.com>
2026-06-10 11:32:25 -0300

    login e cadastro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { Vitrine } from './paginas/vitrine/vitrine';
+import { Login } from './paginas/outh/login/login';
+import { Cadastro } from './paginas/outh/cadastro/cadastro';
 
 export const routes: Routes = [
     {path: "vitrine", component: Vitrine}, 
-    {path: '', redirectTo: "vitrine", pathMatch: 'full'}
+    {path: '', redirectTo: "vitrine", pathMatch: 'full'},
+    {path: "login", component: Login},
+    {path: "cadastro", component: Cadastro}
 ];
