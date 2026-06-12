cc969b515fabd2ac72cdffef270044d9bcdb59bd
Marcus <mv2882106@gmail.com>
2026-06-12 11:01:16 -0300

    site

--- src/app/paginas/outh/login/login.ts
+++ src/app/paginas/outh/login/login.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from "@angular/router";
+
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../../../core/services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  private authservice = inject(AuthService);
+  private router = inject(Router);
+
+  loginForm = new FormGroup({
+    email: new FormControl(""),
+    password: new FormControl("")
+  })
+
+  onSubmit() {
+    const { email, password } = this.loginForm.getRawValue()
+
+    this.authservice.login(email!, password!).subscribe({
+      next: () => alert("Login realizado com sucesso!"),
+      error: () => alert("Erro ao realizar login, verifique suas credenciais!")
+    })
+  }
+}
